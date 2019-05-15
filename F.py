421a6b1cb852b65fd94e59afd8a3fe0f8a245335
Dmitry Ponyatov <dponyatov@gmail.com>
2019-05-15 21:56:59 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- F.py
+++ F.py
@@ -13,6 +13,9 @@ class Frame:
         return self.dump()
     def dump(self,depth=0,prefix=''):
         tree = self.pad(depth) + self.head(prefix)
+        if not depth: Frame.dumped = []
+        if self in Frame.dumped: return tree + ' _/'
+        else: Frame.dumped.append(self)
         for i in self.slot:
             tree += self.slot[i].dump(depth+1,i+' = ')
         for j in self.nest:
@@ -29,11 +32,36 @@ class Frame:
     def __floordiv__(self,that):
         if not isinstance(that,Frame): return self // Frame(that)
         self.nest.append(that) ; return self
-        
+    def __lshift__(self,that):
+        if callable(that): return self << Cmd(that)
+        self[that.val] = that ; return self
+    def __setitem__(self,key,that):
+        self.slot[key] = that ; return self
+
+class Stack(Frame): pass
+
+class Dict(Frame): pass
+
 class Vector(Frame): pass
+
+class Cmd(Frame):
+    def __init__(self,F):
+        Frame.__init__(self, F.__name__)
+        self.fn = F
             
 class File(Frame): pass
 
-argv = Frame('argv')
-for i in sys.argv: argv // i
-print argv
+W = Dict('FORTH') ; W['W'] = W
+
+S = Stack('DATA') ; W['S'] = S
+
+W << ( Vector('ARGV') // File(sys.argv[0]) )
+for i in sys.argv[1:]: W['ARGV'] // i
+                      
+def REPL():
+    while True:
+        print W
+        S // raw_input('\nok> ')
+W << REPL
+REPL()
+
