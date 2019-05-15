c13642ba22398f8781a14895d7b8beace4c2704b
Dmitry Ponyatov <dponyatov@gmail.com>
2019-05-15 11:03:42 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- z.py
+++ z.py
@@ -12,22 +12,38 @@ class Frame:
         return self.dump()
     def dump(self,depth=0,prefix=''):
         tree = self.pad(depth) + self.head(prefix)
+        for i in self.slot:
+            tree += self.slot[i].dump(depth+1,i+' = ')
+        for j in self.nest:
+            tree += j.dump(depth+1)
         return tree
     def head(self,prefix=''):
-        return '%s<%s:%s> /%i @%x' % (prefix,self.type,self.val,self.ref,id(self))
+        return '%s<%s:%s> /%i @%x' % \
+            (prefix,self.type,self.val,self.ref,id(self))
     def pad(self,n):
         return '\n' + '\t' * n
     
     def __floordiv__(self,that):
         self.nest.append(that)
+    def __setitem__(self,key,obj):
+        if callable(obj): self[key] = Cmd(obj) ; return self
+        self.slot[key] = obj ; return self
+
+class Active(Frame): pass
         
-class VM(Frame):
+class Cmd(Active):
+    def __init__(self,F):
+        Active.__init__(self, F.__name__)
+        self.fn = F
+        
+class VM(Active):
+    def __init__(self,V):
+        Active.__init__(self, V)
+        self['REPL'] = self.repl
     def interpret(self):
         lexer = lexer.input( self.pop().val )
         while True:
             if not self.word(): break
-            
-            
     def repl(self):
         while True:
             print self
