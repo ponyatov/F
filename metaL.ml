2c16b7813492828ca0fd286343db8a9b0d9705da
Dmitry Ponyatov <dponyatov@gmail.com>
2019-05-16 15:58:01 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- metaL.ml
+++ metaL.ml
@@ -2,8 +2,8 @@
 'homoiconic metaprogramming language /on top of Python/'	`TITLE		!
 'Dmitry Ponyatov <<dponyatov@gmail.com>>'					`AUTHOR		!
 'CC BY-NC-ND'												`LICENSE	!
-'github: https://github.com/ponyatov/F'						`GITHUB		!
-'wiki: https://github.com/ponyatov/F/wiki'					`WIKI		!
+'https://github.com/ponyatov/F'								`GITHUB		!
+'https://github.com/ponyatov/F/wiki'						`WIKI		!
 
 `README.md										file dup	`README		!
 '# '	MODULE	+														//
