1e9550761b9c6b69adf2bce9e414a1dafeb4e03d
Dmitry Ponyatov <dponyatov@gmail.com>
2019-05-16 09:19:08 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- metaL.ml
+++ metaL.ml
@@ -1,17 +1,5 @@
 `metaL														`MODULE		!
 'homoiconic metaprogramming language /on top of Python/'	`TITLE		!
-''														dup `ABOUT		!
-	'* written on top of Python'
-		'* very light and fast runtime'								//
-		'* portable'												//
-		'* easy to write extensions and integrate extra libraries'	//
-	//
-	'* for embedded development'
-		'* Cortex-M'												//
-		'* MSP430'													//
-		'* embedded Linux'											//
-	//
-	'* based on FORTH'												//
-.
 'Dmitry Ponyatov <<dponyatov@gmail.com>> CC BY-NC-ND'		`LICENSE	!
 'github: https://github.com/ponyatov/F'						`GITHUB		!
+'wiki: https://github.com/ponyatov/F/wiki'					`WIKI		!
