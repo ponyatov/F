2aa97c1890942ed0c1a3605d92bdf2dc1d4d8038
Dmitry Ponyatov <dponyatov@gmail.com>
2025-01-22 17:31:01 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/cli/inc/cli.hpp
+++ lib/cli/inc/cli.hpp
@@ -8,18 +8,26 @@
 /// @brief CLI: Command Line Interface
 /// @ingroup core
 /// @{
-
+/// @name lexer
+/// @{
 extern int yylex();
 extern int yylineno;
 extern char* yytext;
 extern char* yyfile;
 extern FILE* yyin;
+/// @}
+/// @name parser
+/// @{
 extern int yyparse();
 extern void yyerror(const char* msg);
 #include "cli.parser.hpp"
+/// @}
+/// @name lexer
+/// @{
 #define TOKEN(C, X)               \
     {                             \
         yylval.o = new C(yytext); \
         return X;                 \
     }
+/// @}
 /// @}
