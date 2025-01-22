e30ec83513879aa4fa6cead13217beeb5f1e60da
Dmitry Ponyatov <dponyatov@gmail.com>
2025-01-22 16:14:17 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/install.mk
+++ mk/install.mk
@@ -4,12 +4,13 @@ update: $(HOST)_update
 ref: $(RF)
 gz:  $(GZ)
 
-Debian_install: update doc ref gz
+Debian_install: Debian_update doc ref gz
 	sudo dpkg --add-architecture i386
 Debian_update:
 	sudo apt update
 	sudo apt install -uy `cat apt.$(HOST)`
 
-Msys_install:
+Msys_install: doc ref gz
+	pacman -Suy
 Msys_update:
 	pacman -S $(shell cat apt.$(HOST) | tr '\n' ' ')
