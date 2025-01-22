b3943eb2386fe24516796830ca41e3c3e058bd28
Dmitry Ponyatov <dponyatov@gmail.com>
2025-01-22 17:40:18 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/all.mk
+++ mk/all.mk
@@ -1,5 +1,5 @@
 # all
 .PHONY: all run
-all: bin/$(BINFILE) $(S)
-run: bin/$(BINFILE) $(S)
+all: bin/$(BINFILE)$(EXE) $(S)
+run: bin/$(BINFILE)$(EXE) $(S)
 	$^
--- mk/rule.mk
+++ mk/rule.mk
@@ -1,8 +1,8 @@
-bin/$(BINFILE): $(C) $(H) $(CP) $(HP)
+bin/$(BINFILE)$(EXE): $(C) $(H) $(CP) $(HP)
 # $(CXX) $(CFLAGS) -o $@ $(C) $(CP) $(L)
 	cmake           --preset mingw -S . -B tmp/mingw
 	cmake --build   --preset mingw         tmp/mingw
-	cmake --install --preset mingw         tmp/mingw
+	cmake --install                        tmp/mingw
 $(TMP)/%.lexer.cpp: $(LIB)/cli/src/%.lex
 	flex -o $@ $<
 $(TMP)/%.parser.cpp: $(LIB)/cli/src/%.yacc
