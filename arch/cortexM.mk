a6e1ecdc4bd85ebc05f433ee9f6e4215c8c8655f
Dmitry Ponyatov <dponyatov@gmail.com>
2025-01-29 01:43:05 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- arch/cortexM.mk
+++ arch/cortexM.mk
@@ -2,31 +2,3 @@ OS  = bare
 EXE = .elf
 
 TARGET = arm-none-eabi
-
-TCC  = $(TARGET)-gcc
-TCXX = $(TARGET)-g++
-TLD  = $(TCC)
-TAS  = $(TCC) -x assembler-with-cpp
-TCP  = $(TARGET)-objcopy
-TSZ  = $(TARGET)-size
-
-CPUFLAGS += -mthumb
-CFLAGS   += -DHSI_VALUE=8000000
-CFLAGS   += -DVDD_VALUE=3300
-CFLAGS   += -DUSE_FULL_LL_DRIVER
-
-CFLAGS   += -ffunction-sections -fdata-sections
-
-CXXFLAGS += -fno-rtti -fno-exceptions
-# CXXFLAGS += -nostdinc++
-CFLAGS   += -nodefaultlibs
-# CFLAGS   += -nostdlib
-
-# CFLAGS  += -MMD -MP -MF"$(@:%.o=%.d)"
-
-LDFLAGS += -specs=nano.specs
-LDFLAGS += -Thw/$(HW)/$(CPU)x_FLASH.ld
-LDFLAGS += -Wl,--gc-sections
-
-L += -lc -lm -lnosys
-
