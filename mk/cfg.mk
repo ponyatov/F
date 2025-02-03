93fe2835a51cada616b65339ee3870dfa3084079
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-03 20:27:56 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/cfg.mk
+++
@@ -1,8 +0,0 @@
-CFLAGS   += -I$(INC) -I$(TMP) -Og -ggdb
-CCFLAGS  += -std=gnu17
-CXXFLAGS += -std=gnu++17
-
-CFLAGS   += -I$(CWD)/hw/$(HW)/inc     -I$(CWD)/hw/inc
-CFLAGS   += -I$(CWD)/cpu/$(CPU)/inc   -I$(CWD)/cpu/inc
-CFLAGS   += -I$(CWD)/arch/$(ARCH)/inc -I$(CWD)/arch/inc
-CFLAGS   += -I$(CWD)/os/$(OS)/inc	  -I$(CWD)/os/inc
