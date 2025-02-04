ca848c6bc70dd33ede64af70a8147be92d88bfd9
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-04 17:50:32 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,5 @@ void arg(int argc, char *argv) {
 #ifdef POSIX
     fprintf(stderr, "\targ[%i] = <%s>\n", argc, argv);
 #endif
-    //     if (argc >= 1) {
-    //         yyfile = argv;
-    // #ifdef POSIX
-    //         assert(yyin = fopen(yyfile, "r"));
-    //         yyparse();
-    //         fclose(yyin);
-    // #else  // POSIX
-    // #error MCU parser not implemented
-    // #endif  // POSIX
-    //         yyfile = nullptr;
-    //     }
+    if (argc >= 1) cli(argv);
 }
