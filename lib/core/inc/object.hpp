5a1586dc164ac8439362b4d5d848fc3b77d961cb
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-25 12:47:08 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ lib/core/inc/object.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "types.hpp"
+
+/// @defgroup object object
+/// @ingroup core
+/// @brief root class for all objects
+/// @{
+
+/// @brief root class for all objects
+class Object {
+   public:
+    Object();   ///< construct any object
+    ~Object();  ///< some clean up
+};
+
+/// @}
