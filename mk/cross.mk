5ae8a6bb8ae67a28258529b629b108000e493733
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-27 22:49:38 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/cross.mk
+++ mk/cross.mk
@@ -1,11 +1,12 @@
 # HW ?= pc
 # HW ?= mega2560
 # HW ?= pillF030
-HW ?= pillF103
 # HW ?= f4disco
 # HW ?= iskra
 # HW ?= l496disco
 # HW ?= pi800
+# HW ?= pillF103
+HW ?= lm3s6
 
 ELF     = $(BIN)/$(BINFILE).elf
 DFU     = $(BIN)/$(BINFILE).dfu
