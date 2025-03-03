9963e2c00d4e6eb11761577dea13b80a79e2d2ad
Dmitry Ponyatov <dponyatov@gmail.com>
2025-03-03 14:17:32 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- fortex/src/main.rs
+++ fortex/src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 
 use panic_halt as _;
+// use panic_semihosting as _;
 
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
@@ -12,5 +13,6 @@ use fortex::*;
 fn main() -> ! {
     hprintln!("Hello, world!");
     nop();
+    assert_eq!(1, 2);
     halt();
 }
